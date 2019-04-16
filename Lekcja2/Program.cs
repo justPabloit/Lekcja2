1549680afb04e0509e1fe690adb795f058ac1e79
Paweł Piekutowski <futureiscode@gmail.com>
2019-04-16 15:38:20 +0200

    Operatory arytmetyczne i logiczne

--- Lekcja2/Program.cs
+++ Lekcja2/Program.cs
@@ -10,6 +10,51 @@ namespace Lekcja2
     {
         static void Main(string[] args)
         {
+            //int a = 5;
+            //int b = 9;
+            //int c;
+            //int d;
+            //c = a + b;
+            //a += b;
+
+            //d = b % a;
+
+            //Console.WriteLine(c);
+            //Console.WriteLine(d);
+            //// inkrementacja
+            //a++;
+            //Console.WriteLine(a);
+            // operatory logiczne
+            //bool a = 2 > 1;
+            //bool b = 2 == 2;
+            //bool c = 2 != 2;
+            //bool d = !(2 == 2);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+            //Console.WriteLine(d);
+
+            //int a = 2 + 2 * 2;
+            //Console.WriteLine(a);
+
+            bool a = 1 == 1 && 1!=1;// I, AND
+            bool b = 1 == 1 || 2 != 2;// LUB , OR
+            bool c = true || true && false;
+            bool d = (true || true) && false;
+            
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(Math.Pow(2, 3));
+            Console.WriteLine(Math.Sqrt(9));
+            Console.WriteLine(Math.Abs(-5));
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
